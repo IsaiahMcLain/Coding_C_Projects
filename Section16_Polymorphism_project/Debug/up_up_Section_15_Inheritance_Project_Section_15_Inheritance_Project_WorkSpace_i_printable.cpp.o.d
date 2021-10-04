4ec044031079516c9bd69f69e5b3ef4d24fc34f6
Isaiah McLain <70073013+IsaiahMcLain@users.noreply.github.com>
2021-10-04 12:00:32 -0700

    Delete Section16_Polymorphism_project/Debug directory

--- Section16_Polymorphism_project/Debug/up_up_Section_15_Inheritance_Project_Section_15_Inheritance_Project_WorkSpace_i_printable.cpp.o.d
+++
@@ -1,5 +0,0 @@
-Debug/up_up_Section_15_Inheritance_Project_Section_15_Inheritance_Project_WorkSpace_i_printable.cpp.o: \
- ../../Section_15_Inheritance_Project/Section_15_Inheritance_Project_WorkSpace/i_printable.cpp \
- ../../Section_15_Inheritance_Project/Section_15_Inheritance_Project_WorkSpace/i_printable.h
-
-../../Section_15_Inheritance_Project/Section_15_Inheritance_Project_WorkSpace/i_printable.h:
